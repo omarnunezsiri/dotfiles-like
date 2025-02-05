de943e920f922dd6a6214fee5448df9cc437a425
Omar N <menesiena@proton.me>
2025-02-05 12:13:32 -0500

    feat(linux-scrips): add proton vpn installation

--- Fedora/scripts/setup.sh
+++ Fedora/scripts/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Ensure packages are up-to-date
 sudo dnf update
 sudo flatpak update
@@ -14,6 +16,9 @@ sudo systemctl restart systemd-resolved
 sudo systemctl restart NetworkManager
 resolvectl status
 
+# Install tray icons extension
+sudo dnf install libappindicator-gtk3 gnome-shell-extension-appindicator gnome-extensions-app
+
 # GH cli
 sudo dnf install dnf5-plugins
 sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
@@ -22,6 +27,15 @@ sudo dnf install gh --repo gh-cli
 # Essentials
 sudo flatpak install syncthingy
 sudo flatpak install librewolf
+
+## Proton VPN
+wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.2-1.noarch.rpm"
+sudo dnf install ./protonvpn-stable-release-1.0.2-1.noarch.rpm && sudo dnf check-update --refresh 
+sudo dnf install proton-vpn-gnome-desktop 
+# GPG key 0xF43719CA: Userid : “Proton Technologies AG <opensource@proton.me>” Fingerprint: 9E72 DFDF 2AF0 19F0 CCFC 68F3 8DD4 8989 F437 19CA
+# GPG key 0x67FCBBBB:: Userid : “Proton Technologies AG <opensource@proton.me>” Fingerprint: F200 18AD 1914 638B C3B4 85F7 413E 4BA3 67FC BBBB
+
+
 ## VBox
 sudo dnf config-manager addrepo --from-repofile=https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
 sudo dnf update
