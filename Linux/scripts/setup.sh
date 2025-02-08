76d63ebba64a136d072c2f215e830905229d50be
Omar N <menesiena@proton.me>
2025-02-08 04:01:12 -0500

    refactor(linux): add gh cli installation for debian systems

--- Linux/scripts/setup.sh
+++ Linux/scripts/setup.sh
@@ -4,9 +4,14 @@
 sudo apt update && sudo apt-upgrade
 
 # GH cli
-# sudo dnf install dnf5-plugins
-# sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
-# sudo dnf install gh --repo gh-cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
 
 # Essentials
 # sudo flatpak install syncthingy
